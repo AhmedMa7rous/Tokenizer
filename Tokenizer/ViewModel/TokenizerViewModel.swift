c33eb0d6b31799cc6479ddf010ccfc0653ade044
Ahmed Mahrous <ahmed.mahrous@gomini-app.com>
2024-01-07 20:13:15 +0200

    Full app with UI & unit tests

---
+++ Tokenizer/ViewModel/TokenizerViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  TokenizerViewModel.swift
+//  Tokenizer
+//
+//  Created by Ahmed Mahrous on 06/01/2024.
+//
+
+import Foundation
--- TokenizerTests/TokenizerViewModelTests.swift
+++ TokenizerTests/TokenizerViewModelTests.swift
@@ -1,14 +1,13 @@
 //
-//  TokenizerTests.swift
+//  TokenizerViewModelTests.swift
 //  TokenizerTests
 //
-//  Created by Ahmed Mahrous on 06/01/2024.
+//  Created by Ahmed Mahrous on 07/01/2024.
 //
 
 import XCTest
-@testable import Tokenizer
 
-final class TokenizerTests: XCTestCase {
+final class TokenizerViewModelTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
